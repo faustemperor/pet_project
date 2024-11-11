5a653c00a3d210663eb359c46d87b2fb0d63d712
faustemperor <razinkola81@gmail.com>
2024-11-11 10:24:18 +0300

    Добавлен пункт 4 в меню

---
+++ main.py
@@ -0,0 +1,31 @@
+def option_one():
+    print("Вы выбрали пункт 1")
+
+def option_two():
+    print("Вы выбрали пункт 2")
+
+def option_three():
+    print("Вы выбрали пункт 3")
+
+def main():
+    while True:
+        print("\nГлавное меню:")
+        print("1. Пункт 1")
+        print("2. Пункт 2")
+        print("3. Пункт 3")
+        print("0. Выход")
+
+        choice = input("Выберите пункт меню: ")
+        if choice == '1':
+            option_one()
+        elif choice == '2':
+            option_two()
+        elif choice == '3':
+            option_three()
+        elif choice == '0':
+            break
+        else:
+            print("Неверный ввод, попробуйте снова.")
+
+if __name__ == "__main__":
+    main()
