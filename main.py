0479abf127d4ea0d057fbc59237b9b9e5a829883
faustemperor <razinkola81@gmail.com>
2024-11-11 10:34:25 +0300

    4 пункт в меню(1)

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ def option_two():
 def option_three():
     print("Вы выбрали пункт 3")
 
+def option_four():
+    print("Вы выбрали пункт 4")
+
 def main():
     while True:
         print("\nГлавное меню:")
         print("1. Пункт 1")
         print("2. Пункт 2")
         print("3. Пункт 3")
+        print("4. Пункт 4")
         print("0. Выход")
 
         choice = input("Выберите пункт меню: ")
@@ -22,6 +26,8 @@ def main():
             option_two()
         elif choice == '3':
             option_three()
+        elif choice == '4':
+            option_four()
         elif choice == '0':
             break
         else:
